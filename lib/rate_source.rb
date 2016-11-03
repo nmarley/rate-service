7291c0948629e170cb652e649a1222f91397e34a
Nathan Marley <nathan.marley@gmail.com>
2016-11-03 16:37:20 -0700

    add rate_source class & subclasses

---
+++ lib/rate_source.rb
@@ -0,0 +1,104 @@
+require 'faraday'
+require 'uri'
+require 'redis'
+require 'json'
+require 'pp'
+require 'awesome_print'
+require 'byebug'
+
+class RateSource
+  attr_reader :redis
+
+  def initialize(redis)
+    @redis = redis
+  end
+
+  def fetch
+    u = URI.parse(api_url)
+    url = u.scheme + "://" + u.host
+
+    conn = Faraday.new(url: url) do |faraday|
+      faraday.request  :url_encoded
+      faraday.response :logger
+      faraday.adapter  Faraday.default_adapter # Net::HTTP
+    end
+    resp = conn.get u.request_uri
+
+    return post_process(resp.body)
+  end
+
+  def post_process(data)
+    return data
+  end
+
+  def load(hash)
+    hash.each do |key, val|
+      redis.set key, val
+    end
+  end
+
+  def api_url; end
+end
+
+class PoloniexAPI < RateSource
+  API_URL = 'https://poloniex.com/public?command=returnTicker'
+
+  def api_url
+    return API_URL
+  end
+
+  def post_process(data)
+    h = JSON.parse(data)
+    newhash = Hash.new
+
+    # ignore non-BTC markets
+    h.keep_if { |k,v| k.match(/^BTC_/) }
+    h.each do |k,v|
+      newkey = k
+      newval = v['last']
+      newhash[newkey] = newval
+    end
+
+    return newhash
+  end
+end
+
+class BitcoinAverageAPI < RateSource
+  API_URL = 'https://apiv2.bitcoinaverage.com/constants/exchangerates/global'
+
+  def api_url
+    return API_URL
+  end
+
+  def post_process(data)
+    h = JSON.parse(data)
+    newhash = Hash.new
+
+    # remove precious metals and SDRs
+    %w[ XAG XAU XDR XPD XPT ].each do |tick|
+      h['rates'].delete(tick)
+    end
+
+    h['rates'].each do |k,v|
+      newkey = "USD_#{k}"
+      newval = v['rate']
+      newhash[newkey] = newval
+    end
+
+    return newhash
+  end
+end
+
+
+redis = Redis.new
+
+data_source_klasses = [ BitcoinAverageAPI, PoloniexAPI ]
+
+# these fetches can possibly be parallelized
+data_source_klasses.each do |klass|
+  byebug
+  source = klass.new(redis)
+  data = source.fetch
+  source.load(data)
+end
+
