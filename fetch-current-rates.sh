ba688f95d89a263e073e8e900cb9978750d35463
Nathan Marley <nathan.marley@gmail.com>
2016-11-03 13:46:47 -0700

    use APIv2 of BitcoinAverage

--- fetch-current-rates.sh
+++ fetch-current-rates.sh
@@ -3,4 +3,7 @@
 curl https://poloniex.com/public?command=returnTicker | json_pp > polo.json
 curl https://api.bitcoinaverage.com/ticker/global/all | json_pp > ba_global_all.json
 
+# BA v2
+curl https://apiv2.bitcoinaverage.com/constants/exchangerates/global | json_pp > ba2_global_all.json
+
 
