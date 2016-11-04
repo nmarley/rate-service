514a8fcfe2e56b0226210b6a724458a0b49230b4
Nathan Marley <nathan.marley@gmail.com>
2016-11-04 13:19:54 -0700

    update test

---
+++ test/test_rate_helpers.rb
@@ -0,0 +1,36 @@
+require 'test_helper'
+require 'minitest/autorun'
+require 'rate_helpers'
+
+class TestRateHelpers < Minitest::Test
+  include RateHelpers
+  attr_reader :fiat_tickers, :crypto_tickers
+
+  def setup
+    $redis = Redis.new
+    @fiat_tickers = %w[ CNY USD CAD ZAR HKD ]
+    @crypto_tickers = %w[ BTC LTC DASH XMR MAID ETH ]
+  end
+
+  def test_is_fiat
+    @fiat_tickers.each do |ticker|
+      assert is_fiat(ticker)
+    end
+
+    @crypto_tickers.each do |ticker|
+      assert !is_fiat(ticker)
+    end
+  end
+
+  def test_is_crypto
+    @crypto_tickers.each do |ticker|
+      assert is_crypto(ticker)
+    end
+
+    @fiat_tickers.each do |ticker|
+      assert !is_crypto(ticker)
+    end
+  end
+end
+
+
