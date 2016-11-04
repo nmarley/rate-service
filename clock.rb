9497084350370baf0af171c35c6a0885e9a93bbc
Nathan Marley <nathan.marley@gmail.com>
2016-11-03 18:05:13 -0700

    update clock file

--- clock.rb
+++ clock.rb
@@ -1,7 +1,12 @@
 require 'clockwork'
+# /Users/nmarley/Dash/code/rate-service/app/workers
+$:.push('lib')
+$:.push('app/workers')
+require 'bitcoin_average_worker'
+require 'poloniex_worker'
 
 module Clockwork
-  every 1.minute do
+  every 1.minute, 'workers' do
     BitcoinAverageWorker.perform_async
     PoloniexWorker.perform_async
   end
