7b88ad611fe59d21d6bbd55ff77d9c47c17d6115
Nathan Marley <nathan.marley@gmail.com>
2016-11-05 17:22:48 -0700

    remove deprecated bitcoinaverage v1 api

--- fetch-current-rates.sh
+++ fetch-current-rates.sh
@@ -1,7 +1,7 @@
 #! /usr/local/bin/bash
 
 curl https://poloniex.com/public?command=returnTicker | json_pp > polo.json
-curl https://api.bitcoinaverage.com/ticker/global/all | json_pp > ba_global_all.json
+#curl https://api.bitcoinaverage.com/ticker/global/all | json_pp > ba_global_all.json
 
 # BA v2
 curl https://apiv2.bitcoinaverage.com/constants/exchangerates/global | json_pp > ba2_global_all.json
