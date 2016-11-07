093d6aeb85d83f2229ebce69b361c4a3e9bcbd14
Nathan Marley <nathan.marley@gmail.com>
2016-11-07 00:15:30 -0800

    move old fetch script, only used for testing now

--- test/fixture-data/fetch-current-rates.sh
+++ test/fixture-data/fetch-current-rates.sh
@@ -1,9 +1,7 @@
 #! /usr/local/bin/bash
 
+# to fetch new/updated test data:
 curl https://poloniex.com/public?command=returnTicker | json_pp > polo.json
-#curl https://api.bitcoinaverage.com/ticker/global/all | json_pp > ba_global_all.json
-
-# BA v2
 curl https://apiv2.bitcoinaverage.com/constants/exchangerates/global | json_pp > ba2_global_all.json
 
 
